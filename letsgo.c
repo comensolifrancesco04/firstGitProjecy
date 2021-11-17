72096900b81619749ce4fd5b55507727594d6192
comensolifrancesco04 <francesco.comensoli04@gmail.com>
2021-11-17 09:48:18 +0100

    Added comment line 4 from rep2

--- letsgo.c
+++ letsgo.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-int main(){
+int main(){	//Added omment from repository 2
 	printf("Git, let's go!!\n");
 	printf("Added line 1\n");
 	printf("Working on a new feature 1 \n");
